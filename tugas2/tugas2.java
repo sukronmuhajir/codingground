f13ad0f8a2a9d3d33eb4100b43cec1ee002a0d89
sukronmuhajir <sukronmuhajir@gmail.com>
2017-02-27 09:13:03 +0700

    Created by Coding Ground Client

---
+++ tugas2/tugas2.java
@@ -0,0 +1,9 @@
+public class tugas2{
+
+     public static void main(String []args){
+        String[] nama = {"sukron", "muhajir","broden"};
+        for (int i = 0; i < nama.length; i++) {
+         System.out.println(nama[i] + " ");
+      }
+     }
+}
