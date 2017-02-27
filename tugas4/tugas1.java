615db7234571f8c1de9d0c6f856829416c386892
sukronmuhajir <sukronmuhajir@gmail.com>
2017-02-27 09:39:05 +0700

    Created by Coding Ground Client

---
+++ tugas4/tugas1.java
@@ -0,0 +1,10 @@
+public class tugas1{
+
+     public static void main(String []args){
+       String nama ="sukron muhajir";
+       String kota ="jepara";
+       System.out.println("Nama : "+nama+ "    kota   :  " +   kota);
+        
+        
+     }
+}
